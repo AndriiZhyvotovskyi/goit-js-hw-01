649f3a3147590d2923b0a7dce53517e5d13fa663
Andrii Zhyvotovskyi <zhyvotovskyiandrii@gmail.com>
2020-07-06 20:08:42 +0300

    finaly save

--- js/task-5.js
+++ js/task-5.js
@@ -1,52 +1,50 @@
 "use strict";
 
-const input = prompt("введите страну");
 let message;
+let price;
 
-if (input === null) {
+const inputStart = prompt("введите страну");
+
+if (inputStart === null) {
   message = "Отменено пользователем!";
-} else if (input === "") {
-  message = "Вы ничего не указали";
+  console.log(message);
+} else if (inputStart === "") {
+  message = "Вы ввели пустую строку";
+  console.log(message);
 } else {
-  message = "Эта страна не доступна";
+  let country = inputStart.toLowerCase();
+  let inputNormalize;
+  inputNormalize =
+    inputStart[0].toUpperCase() + inputStart.slice(1).toLowerCase();
+
+  switch (country) {
+    case "китай":
+      price = 100;
+      message = `Доставка в ${inputNormalize} будет стоить ${price} кредитов`;
+      break;
+
+    case "чили":
+      price = 250;
+      message = `Доставка в ${inputNormalize} будет стоить ${price} кредитов`;
+      break;
+
+    case "австралия":
+      price = 170;
+      message = `Доставка в ${inputNormalize} будет стоить ${price} кредитов`;
+      break;
+
+    case "индия":
+      price = 80;
+      message = `Доставка в ${inputNormalize} будет стоить ${price} кредитов`;
+      break;
+
+    case "ямайка":
+      price = 120;
+      message = `Доставка в ${inputNormalize} будет стоить ${price} кредитов`;
+      break;
+
+    default:
+      alert("В вашей стране доставка не доступна");
+  }
+  console.log(message);
 }
-
-console.log(message);
-
-// const inputNormalize = input[0].toUpperCase() + input.slice(1).toLowerCase();
-// console.log(inputNormalize);
-
-// const country = input;
-let result;
-
-switch (input) {
-  case "китай":
-    price = 100;
-    result = `Доставка в ${input} будет стоить ${price} кредитов`;
-    break;
-
-  case "чили":
-    price = 250;
-    result = `Доставка в ${input} будет стоить ${price} кредитов`;
-    break;
-
-  case "австралия":
-    price = 170;
-    result = `Доставка в ${input} будет стоить ${price} кредитов`;
-    break;
-
-  case "индия":
-    price = 80;
-    result = `Доставка в ${input} будет стоить ${price} кредитов`;
-    break;
-
-  case "ямайка":
-    price = 120;
-    result = `Доставка в ${input} будет стоить ${price} кредитов`;
-    break;
-
-  default:
-    result = "неверный ввод";
-}
-
-console.log(result);
---
+++ js/task-6.js
@@ -0,0 +1,25 @@
+"use strict";
+
+let total = 0;
+let input;
+
+do {
+  let input = prompt("Введите число");
+
+  if (input === null) {
+    console.log("Отменено пользователем!");
+    break;
+  }
+
+  input = Number(input);
+  const checkForNaN = isNaN(input);
+
+  if (checkForNaN) {
+    alert("Было введено не число, попробуйте еще раз");
+    continue;
+  }
+
+  total += input;
+} while (input !== null);
+
+alert(`Общая сумма чисел равна ${total}`);
